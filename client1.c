66bca2a6a02cc2479fdb1b58a2e93d0d223e4229
nurinnad <97892579+nurinnad@users.noreply.github.com>
2022-06-22 17:31:37 +0800

    first commit client 1

---
+++ client1.c
@@ -0,0 +1,13 @@
+import socket
+import errno
+import time
+import random
+from os import system
+
+
+_ = system('clear')
+
+# Choosing Nickname
+print("-------------------------------------------------------------------")
+print("|            Welcome to the Snake and Ladder Game                  |")
+print("-------------------------------------------------------------------")
