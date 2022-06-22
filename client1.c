75b6b7102dea96a6b9fcc1da1c594f86bfa0d1dc
nurinnad <nurinnadiah32@gmail.com>
2022-06-22 09:34:43 +0000

    second commit for client 1

--- client1.c
+++ client1.c
@@ -11,3 +11,9 @@ _ = system('clear')
 print("-------------------------------------------------------------------")
 print("|            Welcome to the Snake and Ladder Game                  |")
 print("-------------------------------------------------------------------")
+print ("Name will display on server")
+nickname = input("Choose your nickname: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('192.168.253.3', 55555))
