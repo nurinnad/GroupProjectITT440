49be9deea9cf8a9b1f7be27f08e537054c7b42ca
nurinnad <nurinnadiah32@gmail.com>
2022-06-27 14:43:27 +0000

    Update file for client1

--- client1.c
+++ client1.c
@@ -17,3 +17,21 @@ nickname = input("Choose your nickname: ")
 # Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.253.3', 55555))
+
+# just of effects. add a delay of 1 second before performing any action
+WAIT  = 1
+MAX_VAL = 50
+
+# snake takes you down
+snakes = {
+    39: 3,
+    46: 14,
+    49: 13,
+}
+
+# ladder takes you up
+ladders = {
+    5: 43,
+    9: 30,
+    20: 41,
+}
